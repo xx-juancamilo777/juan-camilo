9b1e7daa44aaf061157468ffac7d04258f33b3cd
xx-juancamilo777 <juan.gfernandez@mail.escuelaing.edu.co>
2025-09-28 17:54:18 +0000

    commit

--- jij.cpp
+++ jij.cpp
@@ -1,28 +1,45 @@
 #include <iostream>
+#include <string>
+#include <cstdlib> 
 using namespace std;
 
-bool isPalindrome(int x) {
-    // Negative numbers and multiples of 10 (except 0) are not palindromes
-    if (x < 0 || (x % 10 == 0 && x != 0)) {
-        return false;
+float add(float a, float b) { return a + b; }
+float subtract(float a, float b) { return a - b; }
+float multiply(float a, float b) { return a * b; }
+float divide(float a, float b) { return a / b; }
+
+int main(int argc, char* argv[])
+{
+    if (argc != 4) {
+        cerr << "Uso: " << argv[0] << " <operando1> <operador> <operando2>\n";
+        return 1;
     }
 
-    int revertedNumber = 0;
-    while (x > revertedNumber) {
-        int digit = x % 10;           // get last digit
-        revertedNumber = revertedNumber * 10 + digit;
-        x /= 10;                      // remove last digit
+
+    float op1 = atof(argv[1]);
+    float op2 = atof(argv[3]);
+    string operador = argv[2];
+
+    float resultado = 0;
+
+    if (operador == "+") {
+        resultado = add(op1, op2);
+    } else if (operador == "-") {
+        resultado = subtract(op1, op2);
+    } else if (operador == "*" || operador == "x" || operador == "X") {
+        resultado = multiply(op1, op2);
+    } else if (operador == "/") {
+        if (op2 == 0) {
+            cerr << "Error: División por cero.\n";
+            return 1;
+        }
+        resultado = divide(op1, op2);
+    } else {
+        cerr << "Error: Operador no válido. Use +, -, *, x o /.\n";
+        return 1;
     }
 
-    // Check for even and odd length numbers
-    return (x == revertedNumber) || (x == revertedNumber / 10);
-}
-
-int main() {
-    cout << boolalpha; // print true/false instead of 1/0
-    cout << isPalindrome(121) << endl;   // true
-    cout << isPalindrome(-121) << endl;  // false
-    cout << isPalindrome(10) << endl;    // false
-    cout << isPalindrome(12321) << endl; // true
+    cout << "Resultado: " << resultado << endl;
     return 0;
 }
+
